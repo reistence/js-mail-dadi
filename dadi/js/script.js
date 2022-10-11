cf946429e61fbe15a8cd8910241c747c6bb06044
reistence <gvlamberti@gmail.com>
2022-10-11 16:17:05 +0200

    comment added

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -4,15 +4,18 @@ console.log(userHand);
 // user hand printed in index
 const playerUser = document.getElementById("user-hand");
 playerUser.innerHTML = userHand;
+
 // rnd nr for cpu
 const cpuHand = Math.floor(Math.random() * (6 - 1 + 1) + 1);
 console.log(cpuHand);
 // print cpu hand in index
 const playerCpu = document.getElementById("cpu-hand");
 playerCpu.innerHTML = cpuHand;
+
 //grab where the winner will be displayed
 const winner = document.getElementById("winner");
 
+//initialise what will be displayed
 let result = "";
 
 //declare the winner and print it in winner
