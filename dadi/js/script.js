cc03b169ae2f6730a8060b456713ddb7367f4871
reistence <gvlamberti@gmail.com>
2022-10-12 10:43:21 +0200

    play btn + dice animeation added

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,34 +1,38 @@
-// rnd nr for user
-const userHand = Math.floor(Math.random() * (6 - 1 + 1) + 1);
-console.log(userHand);
-// user hand printed in index
-const playerUser = document.getElementById("user-hand");
-playerUser.innerHTML = userHand;
+const playBtn = document.getElementById("play");
 
-// rnd nr for cpu
-const cpuHand = Math.floor(Math.random() * (6 - 1 + 1) + 1);
-console.log(cpuHand);
-// print cpu hand in index
-const playerCpu = document.getElementById("cpu-hand");
-playerCpu.innerHTML = cpuHand;
+playBtn.addEventListener("click", function () {
+  // rnd nr for user
+  const userHand = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+  console.log(userHand);
+  // user hand printed in index
+  const playerUser = document.getElementById("user-hand");
+  playerUser.innerHTML = userHand;
 
-//grab where the winner will be displayed
-const winner = document.getElementById("winner");
+  // rnd nr for cpu
+  const cpuHand = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+  console.log(cpuHand);
+  // print cpu hand in index
+  const playerCpu = document.getElementById("cpu-hand");
+  playerCpu.innerHTML = cpuHand;
 
-//initialise what will be displayed
-let result = "";
+  //grab where the winner will be displayed
+  const winner = document.getElementById("winner");
 
-//declare the winner and print it in winner
-if (userHand > cpuHand) {
-  result = "User won!";
-  winner.innerHTML = result;
-  winner.classList.add("green");
-} else if (userHand < cpuHand) {
-  result = "CPU won!";
-  winner.innerHTML = result;
-  winner.classList.add("red");
-} else {
-  result = "It's a tie!";
-  winner.innerHTML = result;
-  winner.classList.add("grey");
-}
+  //initialise what will be displayed
+  let result = "";
+
+  //declare the winner and print it in winner
+  if (userHand > cpuHand) {
+    result = "User won!";
+    winner.innerHTML = result;
+    winner.classList.add("green");
+  } else if (userHand < cpuHand) {
+    result = "CPU won!";
+    winner.innerHTML = result;
+    winner.classList.add("red");
+  } else {
+    result = "It's a tie!";
+    winner.innerHTML = result;
+    winner.classList.add("grey");
+  }
+});
